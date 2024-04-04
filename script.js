b2641de251d3733af41053459e82095d26bb2c37
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-04-04 23:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // complete the given function
 
 function palindrome(str){
+	let a = "";
+	for (let k of str){
+		if (k!==" "){
+			a+=k;
+		}
+	}
+	let i = 0;
+	let j = a.length;
+	while (j>i){
+		if (a[i]!==a[j]){
+			return false
+		}
+		i++;
+		j--;
+	}
+	return true
+	
 
 }
 module.exports = palindrome
